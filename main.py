9f27c6414c840459d6d653d92e62ec0bb45e3e9f
Vlofer <59262227+Vlofer@users.noreply.github.com>
2023-12-19 15:35:29 +0200

    Hello world

---
+++ main.py
@@ -0,0 +1,9 @@
+def print_hi(name):
+    print(f'Hello {name}')
+    print("How are you?")
+
+if __name__ == '__main__':
+    name = input("Your name: ")
+    print_hi(name)
+
+    print("How is weather?")
