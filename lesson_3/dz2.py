db65851b59ce44651c6f1964ec2b3b7dbae09c3c
Vlofer <59262227+Vlofer@users.noreply.github.com>
2023-12-20 21:24:41 +0200

    переробив трошки, засунув перетворення строки в try а не весь код. І узнав що таке continue

--- lesson_3/dz2.py
+++ lesson_3/dz2.py
@@ -3,21 +3,23 @@ while True:
     #якщо ввести х прекратить роботу
     if user_number == "x":
         break
-#закидуємо це в трай щоб якщо введеться не число або не х ошибка не ламала код
+    #закидуємо це в трай щоб якщо введеться не число або не х ошибка не ламала код
     try:
-        #На цьому етапі перетворюємо букву в число
+        #пробуємо перетворити строку в число
         num = int(user_number)
-        if num == -500 or num < -500:
-                print("your number is less than or equal to -500 ")
-        elif num == -100 or num < -100:
-                print("your number is equals or more than -100, but less than -500")
-        elif num < 0 :
-            print("your number is less 0 or more to -100")
-        elif num == 0 or num < 100:
-            print("your number is equals or more than 0, but less than 100")
-        elif num == 100 or num < 500:
-            print("your number is equals or more than 100, but less than 500")
-        elif num == 500 or num > 500:
-            print("your number is equals or more then 500")
     except ValueError:
+        # якщо це не число то виводимо текст, і починаємо цикл заново continue
         print("not a number or not x")
+        continue
+    if num == -500 or num < -500:
+            print("your number is less than or equal to -500 ")
+    elif num == -100 or num < -100:
+            print("your number is equals or more than -100, but less than -500")
+    elif num < 0 :
+        print("your number is less 0 or more to -100")
+    elif num == 0 or num < 100:
+        print("your number is equals or more than 0, but less than 100")
+    elif num == 100 or num < 500:
+        print("your number is equals or more than 100, but less than 500")
+    elif num == 500 or num > 500:
+        print("your number is equals or more then 500")
