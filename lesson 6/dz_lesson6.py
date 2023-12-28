1dd388f7b706f11a5c17c76b47dac231606866ba
Vlofer <59262227+Vlofer@users.noreply.github.com>
2023-12-28 12:46:41 +0200

    поняв як добавить метод slice

--- lesson 6/dz_lesson6.py
+++ lesson 6/dz_lesson6.py
@@ -9,43 +9,47 @@ def add(notes_list):
     notes_list.append(new_note_input)
 
 
-def earliest(notes_all):
+# Function earliest
+def earliest(notes_all, num_notes):
     """
     Виводіть список з нотатками спочатку старі потім нові
     :param notes_all: Готовий список з нотатками
     :return: Список з нотатками
     """
-    for element in notes_all:
+    for element in notes_all[:num_notes]:
         print(element)
 
 
-def latest (notes_reverse_all):
+# Function latest
+def latest(notes_reverse_all, num_notes):
     """
     Виводить список з нотатками спочатку нові потім старі
     :param notes_reverse_all:  список з тотатками
     :return: перевернутий список
     """
-    for element in reversed(notes_reverse_all):
+    for element in reversed(notes_reverse_all[:num_notes]):
         print(element)
 
 
-def longest(notes_long):
+# Function longest
+def longest(notes_long, num_notes):
     """
     Виводить список з нотатками по довжинні  спочатку самі довгі
     :param notes_long: список з нотатками
     :return: сортирований список
     """
-    for element in sorted(notes_long, key=len, reverse=True):
+    for element in sorted(notes_long, key=len, reverse=True)[:num_notes]:
         print(element)
 
 
-def shortest(notes_shortest):
+# Function shortest
+def shortest(notes_shortest, num_notes):
     """
     Виводить список з нотатками по довжинні спочатку самі короткі
     :param notes_shortest: список з нотатками
     :return: сортирований список
     """
-    for element in sorted(notes_shortest, key=len, reverse=False):
+    for element in sorted(notes_shortest, key=len, reverse=False)[:num_notes]:
         print(element)
 
 
@@ -69,23 +73,37 @@ while True:
 Виберіть що ви хочете зробити
     ''')
     user_input = input('Вибір: ')
+    # try to int user input
     try:
         user_input = int(user_input)
     except ValueError:
         print('''
         |Введіть ціле число|''')
         continue
+
     if 0 < user_input < 7:
+        # func add
         if user_input == 1:
             add(notes)
-        elif user_input == 2:
-            earliest(notes)
-        elif user_input == 3:
-            latest(notes)
-        elif user_input == 4:
-            longest(notes)
-        elif user_input == 5:
-            shortest(notes)
+        # NUM NOTES
+        elif user_input in [2, 3, 4, 5, ]:
+            try:
+                num_notes = int(input("Скільки потрібно показати нотатків: "))
+                if num_notes < 0:
+                    print("Більше 0 введи")
+                    continue
+            except ValueError:
+                print("Числооооо!")
+                continue
+            # Other
+            if user_input == 2:
+                earliest(notes, num_notes)
+            elif user_input == 3:
+                latest(notes, num_notes)
+            elif user_input == 4:
+                longest(notes, num_notes)
+            elif user_input == 5:
+                shortest(notes, num_notes)
         elif user_input == 6:
             exit()
     else:
